0b4c7df7a9850fb7f94992e6faf82cf8548ac33a
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-09-07 17:49:41 +0300

    FIX: refactored the code

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -12,38 +12,41 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-    if (c < 0 || r < 0) 0
-    else if (c == 0 || c == r) 1
+  def pascal(c: Int, r: Int): Int =
+    if c < 0 || r < 0 then 0
+    else if c == 0 || c == r then 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  }
+
 
   /**
    * Exercise 2
    */
-   def balance(chars: List[Char]): Boolean = {
-    def difference(acc: Int, ch: List[Char]): Int = {
+  def balance(chars: List[Char]): Boolean =
+    def difference(acc: Int, ch: List[Char]): Int =
       val head = ch.head
       val tail = ch.tail
       val isLeft = head == '('
       val isRight = head == ')'
       val notEmptyTail = tail.nonEmpty
-      if (acc == 1) -1
-      else if (!isLeft && !isRight)
-        if (notEmptyTail) difference(acc, tail) else acc
-      else if (isLeft)
-        if (notEmptyTail) difference(acc - 1, tail) else acc - 1
-      else if (notEmptyTail) difference(acc + 1, tail) else acc + 1
-    }
+      if acc == 1 then -1
+      else if !isLeft && !isRight then
+        if notEmptyTail then difference(acc, tail)
+        else acc
+      else if isLeft then
+        if notEmptyTail then difference(acc - 1, tail)
+        else acc - 1
+      else if notEmptyTail then difference(acc + 1, tail)
+        else acc + 1
 
     difference(0, chars) == 0
-  }
+
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    if (money > 0 && !coins.isEmpty)
+  def countChange(money: Int, coins: List[Int]): Int =
+    if money > 0 && !coins.isEmpty then
       countChange(money, coins.tail) + countChange(money - coins.head, coins)
-    else if (money == 0) 1 else 0
-  }
+    else if money == 0 then 1
+      else 0
+
